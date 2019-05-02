28fed4073c57a1cdec0adc69388686e149a16a18
jdoeatw <george@georges-air.attlocal.net>
2019-05-01 22:12:09 -0700

    initial commit

---
+++ json_play.py
@@ -0,0 +1,37 @@
+import json
+import pprint
+
+json_string = '''
+
+{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+
+
+'''
+
+d = json.loads((json_string))
+js = json.dumps(d, indent=4)
+
+print(list((d['glossary']["GlossDiv"]["GlossList"]["GlossEntry"]).keys()))
+
+
